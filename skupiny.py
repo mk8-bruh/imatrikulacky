97a1fc130f4d39e49bda30c13e8480a959cf4090
mk8 <153662482+mk8-bruh@users.noreply.github.com>
2024-11-29 10:20:39 +0100

    Update skupiny.py

--- skupiny.py
+++ skupiny.py
@@ -1,70 +1,138 @@
-from math import inf
-from random import randint as rand
+def n2a(n):
+    d, m = divmod(n, 26)
+    return '' if n < 0 else n2a(d - 1) + chr(m + 65)
 
-def parseName(str):
-    if len(str.split()) == 1:
-        n = str.strip()
-        return n[0].upper() + n[1:].lower()
-    else:
-        n = str.split()
-        f, l = n[0].strip(), n[-1].strip()
-        return f[0].upper() + f[1:].lower() + " " + l[0].upper() + l[1:].lower()
+class Student:
+    def __init__(self, data):
+        if not isinstance(data, str):
+            raise TypeError(f"Student data must be a string (received: {type(data)})")
 
-classes = []
-studentByClass = dict()
-classByStudent = dict()
+        s = data.strip().split()
+        n = len(s)
+        if n < 2:
+            raise ValueError("Invalid student data string")
+        s = [w[0].upper() + w[1:] for w in s]
+        self.Class = s[-1]
+        self.FirstName = s[0]
+        if n > 2:
+            self.LastName = s[-2]
+        self.FullName = " ".join(s[:-1])
+        self.Groups = []
+     
+    def __repr__(self):
+    	return f"<[Student] Name: {self.FullName}, Class: {self.Class}, Groups: {[g.GetNamePath() for g in self.Groups]}>"
+    def __str__(self):
+    	return self.__repr__()
+
+class Group:
+    def __init__(self, name = "", subgroups = [], parent = None):
+        if not isinstance(name, str):
+            raise TypeError(f"Group name must be a string (received: {type(name)})")
+        if not isinstance(subgroups, list):
+            raise TypeError(f"Subgroup count must be a list or None (received: {type(subgroups)})")
+
+        self.Name = name
+        self.Subgroups = [Group(str(g), parent = self) for g in subgroups]
+        self.SubgroupByStudent = dict()
+        self.Parent = parent
+        self.Students = []
+        self.Classes = dict()
+    def AddStudent(self, student):
+        if not isinstance(student, Student):
+            raise TypeError(f"Subject must be a Student object (received: {type(student)})")
+
+        student.Groups.append(self)
+        self.Students.append(student)
+        if not student.Class in self.Classes:
+            self.Classes[student.Class] = []
+        self.Classes[student.Class].append(student)
+        if len(self.Subgroups) > 0:
+            s = Group.BestGroup(self.Subgroups, student)
+            if s:
+                s.AddStudent(student)
+                self.SubgroupByStudent[student] = s
+    
+    def GetNamePath(self):
+        return f"{self.Parent.GetNamePath()}/{self.Name}" if self.Parent else self.Name
+    def GetStudentCount(self):
+        return len(self.Students)
+    def GetClassCount(self, className):
+        return len(self.Classes.get(className, []))
+    
+    @staticmethod
+    def BetterGroup(a, b, student):
+        if a != None and not isinstance(a, Group):
+            raise TypeError(f"Argument #1 must be a Group object (received: {type(a)})")
+        if b != None and not isinstance(student, Student):
+            raise TypeError(f"Argument must be a Student object (received: {type(b)})")
+        if student == None or not isinstance(student, Student):
+            raise TypeError(f"Subject must be a Student object (received: {type(student)})")
+
+        return (not b) or (a.GetClassCount(student.Class) < b.GetClassCount(student.Class)) or (a.GetClassCount(student.Class) == b.GetClassCount(student.Class) and a.GetStudentCount() < b.GetStudentCount())
+    @staticmethod
+    def BestGroup(groups, student):
+        best = None
+        for group in groups:
+            if Group.BetterGroup(group, best, student):
+                best = group
+        return best
+
+    def __repr__(self):
+    	return f"<[Group] Name: {self.Name}, Parent: {self.Parent.Name if self.Parent else 'None'}, Path: {self.GetNamePath()}, Subgroups: {[s.Name for s in self.Subgroups]}, Students: {[f'{s.FullName} ({s.Class}) - {self.SubgroupByStudent[s].Name}' for s in self.Students]}>"
+    def __str__(self):
+    	return self.__repr__()
+
+groupCount = int(input("# of groups: "))
+subgroupCount = int(input("# of groups: "))
+
+students = []
+groups = [Group(n2a(i), [str(j + 1) for j in range(subgroupCount)]) for i in range(groupCount)]
+groupsByName = {g.Name.lower(): g for g in groups}
 
 exit = False
-currentClass = None
 while not exit:
-    inp = input(f"{currentClass}> " if currentClass else "$: ")
-    if inp.strip() != "":
-        if currentClass:
-            n = parseName(inp)
-            if not n in studentByClass[currentClass]:
-                studentByClass[currentClass].append(n)
-                classByStudent[n] = currentClass
+    inp = input("> ").strip()
+    if "#" in inp:
+        exit = True
+    elif "?" in inp:
+        n = " ".join([w.lower() for w in inp.split() if w != "?"])
+        matches = []
+        for s in students:
+            if n in s.FullName.lower():
+                matches.append(s)
+        print(f"{len(matches)} matches")
+        for s in matches:
+            print(f"    {s.FullName} ({s.Class}) - {s.Groups[1].GetNamePath()}")
+    elif "*" in inp:
+        g = " ".join([w.lower() for w in inp.split() if w != "*"])
+        if g in groupsByName:
+            gr = groupsByName[g]
+            print(f"  {gr.Name}")
+            for c in gr.Classes:
+                print(f"    {c}")
+                for s in gr.Classes[c]:
+                    print(f"      {s.FullName} - {s.Groups[1].Name}")
+        elif g == "":
+            for g in groups:
+                print(f"  {g.Name}")
+                for c in g.Classes:
+                    print(f"    {c}")
+                    for s in g.Classes[c]:
+                        print(f"      {s.FullName} - {s.Groups[1].Name}")
         else:
-            currentClass = inp.strip()
-            if not currentClass in classes:
-                classes.append(currentClass)
-                studentByClass[currentClass] = []
+            print("  no groups found")
+    elif "$" in inp:
+        e = " ".join([w for w in inp.split() if w != "$"])
+        try:
+            print(f"  {eval(e)}")
+        except Exception as e:
+            print(f"  error: {e}")
     else:
-        if currentClass:
-            currentClass = None
-        else:
-            exit = True
-
-ngroups = int(input("# of groups: "))
-nsgroups = int(input("# of subgroups: "))
-groups = [{"count": 0, "scounts": [0 for _ in range(nsgroups)]} for _ in range(ngroups)]
-sgroupByStudent = {}
-
-for id in classes:
-    cl = studentByClass[id]
-    for _ in range(len(cl)):
-        g, c = None, inf
-        for f in groups:
-            if f["count"] < c:
-                g, c = f, f["count"]
-        s, v = None, inf
-        for i in range(nsgroups):
-            if g["scounts"][i] < v:
-                s, v = i, g["scounts"][i]
-        if not id in g:
-            g[id] = []
-        n = cl.pop(rand(0, len(cl) - 1))
-        g[id].append(n)
-        g["count"] += 1
-        sgroupByStudent[n] = s
-        g["scounts"][s] += 1
-
-for i in range(ngroups):
-    g = groups[i]
-    print(f"group {i + 1} ({g["count"]})")
-    for id in classes:
-        if id in g:
-            print(f"> {id}")
-            for name in sorted(g[id]):
-                print(f"  {name} ({sgroupByStudent[name] + 1})")
-    print("----------")
+        try:
+            s = Student(inp)
+            g = Group.BestGroup(groups, s)
+            g.AddStudent(s)
+            students.append(s)
+            print(f"{', '.join([g.Name for g in s.Groups])}")
+        except Exception as e:
+            print(f"  error: {e}")
